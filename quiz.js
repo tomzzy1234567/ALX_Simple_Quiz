0f72b0a5c2637d08034b93df90315f8278ee2c38
tomzzy1234567 <150262149+tomzzy1234567@users.noreply.github.com>
2025-01-18 11:20:59 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 function checkAnswer() {
-    const correctAnswer = "paris";
+    const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
     if (userAnswer === correctAnswer) {
